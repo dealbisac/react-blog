9739f4b06c46489c9afcda76f688951b7098228c
dealbisac <chand.dipendra19@gmail.com>
2020-10-15 14:20:44 +0545

    menu and home wrapper added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Home from './components/Pages/Home';
+import PageWrapper from './components/PageWrapper';
 
-function App() {
-  return (
-    <div className="app">
-      <h1>Blog</h1>
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <PageWrapper>
+        <Home />
+        <h1>Hello</h1>
+      </PageWrapper>
+    )
+  }
 }
 
 export default App;
