e2294c98503962cf3847433ecc63ee7dc8b3e387
dealbisac <chand.dipendra19@gmail.com>
2020-10-15 14:33:21 +0545

    Link added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import Home from './components/Pages/Home';
 import PageWrapper from './components/PageWrapper';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <PageWrapper>
-        <Home />
-        <h1>Hello</h1>
-      </PageWrapper>
+      <Router>
+        <PageWrapper>
+          <Route
+            path="/"
+            component={Home}
+          />
+        </PageWrapper>
+
+      </Router>
     )
   }
 }
